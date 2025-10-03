2cc6f244c1cfd5d4880771ea8421ea3a55116314
maria-acevedo-bueno <9201975@alumnos.ufv.com>
2025-10-03 23:42:17 +0200

    New attempt at fixing ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    ".azurewebsites.net",  # wildcard: allows any *.azurewebsites.net
+    "relecloud-maria-acevedo-dkfza8h7adf4fncq.francecentral-01.azurewebsites.net",
 ]
 
 # Application definition
