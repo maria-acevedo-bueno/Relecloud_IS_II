140d2ffe846edecf6ee0f65295ac189014c1a771
maria-acevedo-bueno <9201975@alumnos.ufv.com>
2025-10-03 23:19:02 +0200

    Initial commit

---
+++ relecloud/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Destination(models.Model):
+    name = models.CharField(
+        unique = True,
+        null = False,
+        blank = False,
+        max_length = 50
+    )
+    description = models.TextField(
+        max_length = 2000,
+        null = False,
+        blank = False
+    )
---
+++ relecloud/urls.py
@@ -0,0 +1,9 @@
+## APP (relecloud)
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'), #Default, go to index view
+    path('about/', views.about, name='about') #About page
+]
