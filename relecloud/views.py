2b9fab9bf216c45c1d64198e7d531bce98ae86a2
maria-acevedo-bueno <9201975@alumnos.ufv.com>
2025-10-04 19:52:48 +0200

    Implemented crispy forms to add functionalities with destinations. Added whitenoise to manage static files-

--- relecloud/views.py
+++ relecloud/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from . import models
 from django.views import generic
 
@@ -14,11 +15,26 @@ def destinations(request):
     return render(request, 'relecloud/destinations.html', {'destinations': all_destinations})
 
 class DestinationDetailView(generic.DetailView):
-    template_name = 'destination_detail.html'
+    template_name = 'relecloud/destination_detail.html'
     model = models.Destination
     context_object_name = 'destination'
 
+class DestinationCreateView(generic.CreateView):
+    model = models.Destination
+    template_name = 'relecloud/destination_form.html'
+    fields = ['name', 'description']
+
+class DestinationUpdateView(generic.UpdateView):
+    model = models.Destination
+    template_name = 'relecloud/destination_form.html'
+    fields = ['name', 'description']
+
+class DestinationDeleteView(generic.DeleteView):
+    model = models.Destination
+    template_name = 'relecloud/destination_confirm_delete.html'
+    success_url = reverse_lazy('destinations')
+
 class CruiseDetailView(generic.DetailView):
-    template_name = 'cruise_detail.html'
+    template_name = 'relecloud/cruise_detail.html'
     model = models.Cruise
     context_object_name = 'cruise'
