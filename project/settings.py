acd328c6433e05e2ec6ffda73a1b8d9864a7f082
maria-acevedo-bueno <9201975@alumnos.ufv.com>
2025-10-04 12:51:14 +0200

    Fixes

--- project/settings.py
+++ project/settings.py
@@ -118,8 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"       
+STATIC_ROOT = BASE_DIR / "staticfiles"   
+STATICFILES_DIRS = [
+    BASE_DIR / "relecloud/static",   
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
