57e02f2e34f36b54cc5e9882775dca39e4c33b3f
maria-acevedo-bueno <9201975@alumnos.ufv.com>
2025-10-04 16:40:50 +0200

    Add static file serving in development mode

--- project/urls.py
+++ project/urls.py
@@ -19,8 +19,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('relecloud.urls')),  #Includes URL conf from relecloud
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
